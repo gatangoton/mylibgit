9ab42706d36f6a354d708dd093457cf1406fee9a
taketo <reg.t.h@arancia.jp>
2012-05-18 00:08:46 +0900

    bash practice

--- Practice/src/take/Practice.java
+++ Practice/src/take/Practice.java
@@ -1,5 +1,12 @@
 package take;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
 import take.mylib.unit.UnitString;
 
 public class Practice {
@@ -8,6 +15,7 @@ public class Practice {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*
 		System.out.println("Hello world");
 
 		UnitString us = new UnitString((""));
@@ -16,6 +24,36 @@ public class Practice {
 
 		System.out.println(UnitString.convertTo(3000, "psi", "atm"));
 		System.out.println(UnitString.convertTo("NM", "ft*m"));
+		*/
+
+		ProcessBuilder pb = new ProcessBuilder("bash","a.sh");
+		Process p=null;
+		File f=new File("c:\\cygwin\\home\\taketo\\temp");
+		pb.directory(f);
+		System.out.println(f);
+		System.out.println("--------------------------------------");
+		Map <String, String> m = pb.environment();
+		for(String s : m.keySet()){
+			System.out.println(s+"\t"+m.get(s));
+		}
+		System.out.println("--------------------------------------");
+		pb.redirectErrorStream(true);
+		try{
+			p = pb.start();
+
+		InputStream in = p.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String s;
+		while((s=br.readLine()) != null){
+			System.out.println(s);
+		}
+
+		}catch(IOException e){
+			System.out.println("err exit");
+			System.exit(-1);
+		}
+
+		p.destroy();
 	}
 
 }
